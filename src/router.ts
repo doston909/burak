9ba410a821757c8a3f47d6958ac0c793abc2d577
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-06-26 00:31:53 +0900

    feat: create routing and member controller

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,11 @@
 import express, {Request, Response} from 'express';
 const router = express.Router();
+import memberController from './controllers/member.controller';
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Home Page');
-});
+router.get('/', memberController.goHome);
 
-router.get('/login', (req: Request, res: Response) => {
-    res.send('Login Page');
-});
+router.get('/login', memberController.getLogin);
 
-router.get('/signup', (req: Request, res: Response) => {
-    res.send('Signup Page');
-});
+router.get('/signup', memberController.getSignup);
 
 export default router;
