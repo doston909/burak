a6a0a83d9dee25c2c9d1dfc2c481235882e67fbb
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-07-04 23:57:21 +0900

    fix: create sessions on login and signup

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,4 +1,7 @@
+import { ObjectId } from "mongoose";
 import { MemberStatus, MemberType } from "../enums/member.enum";
+import { Request } from "express";
+import { Session } from "express-session";
 
 export interface Member {
     memberType: MemberType;
@@ -29,4 +32,9 @@ export interface MemberInput {
 export interface LoginInput {
     memberNick: string;
     memberPassword: string;
+}
+
+export interface AdminRequest extends Request {
+    member: Member;
+    session: Session & { member: Member };
 }
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -55,18 +55,16 @@ private readonly memberModel;
     const exist = await this.memberModel
      .findOne({memberType: MemberType.RESTAURANT})
      .exec();
-     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED)
+     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
      
-     console.log("before:", input.memberPassword);
      const salt = await bcrypt.genSalt();
      input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
-     console.log("after:", input.memberPassword);
      try {
         const result = await this.memberModel.create(input);
         result.memberPassword = "";
         return result;
      }  catch (err) {
-        throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED)
+        throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
      }
     }
 
